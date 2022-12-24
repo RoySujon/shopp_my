21aeb330cdba2b28268fe8c28af1a1295e79059d
Roy <goutomroy770@gmail.com>
2022-12-25 05:12:31 +0600

    wjekd

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 // import 'package:flutter/gestures.dart';
-//riaz
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:shopp_my/utls/const.dart';
